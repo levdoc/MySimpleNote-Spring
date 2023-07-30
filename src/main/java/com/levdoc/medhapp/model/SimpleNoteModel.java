1e0c88a8ad5b4d770d5529b885dcfbb662e1269e
levdoc <levdoc@live.ru>
2023-07-30 20:58:05 +0300

    H2 DB Connection Settings + SimpleNoteModel

--- src/main/java/com/levdoc/medhapp/model/SimpleNoteModel.java
+++ src/main/java/com/levdoc/medhapp/model/SimpleNoteModel.java
@@ -1,4 +1,27 @@
 package com.levdoc.medhapp.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "notes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SimpleNoteModel extends GenericModel{
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "main_text", nullable = false)
+    private String mainText;
+
+    @Column(name = "publish_date", nullable = false)
+    private LocalDate publishDate;
+
 }
