c4f8a6f1aa3460f7bbbc56c1f8cf43d63ba07a16
levdoc <levdoc@live.ru>
2023-09-12 22:08:39 +0300

    EmExcelExporter

--- src/main/java/com/levdoc/medhapp/service/EmExcelExporter.java
+++ src/main/java/com/levdoc/medhapp/service/EmExcelExporter.java
@@ -1,15 +1,33 @@
 package com.levdoc.medhapp.service;
 
+import groovy.util.logging.Log4j2;
+import lombok.extern.log4j.Log4j;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service
+
 public class EmExcelExporter {
     @Value("classpath:file/tmp/xlsxTemplateEm.xlsx")
     private Resource xlsxTemplateEm;
+    private XSSFWorkbook workbook;
 
     public void testMethod() {
+
+        try {
+            workbook = new XSSFWorkbook(xlsxTemplateEm.getFile());
+        } catch (IOException | InvalidFormatException e) {
+            System.out.println(e.getLocalizedMessage());
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(workbook.getNumberOfSheets());
+
         System.out.println(xlsxTemplateEm.isFile());
     }
 
